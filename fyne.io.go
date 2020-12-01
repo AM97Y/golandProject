62a972c3f295b3f46fcfec586a3e65b2d698c743
monsterpc <maverina518@gmail.com>
2020-12-01 11:42:46 +0300

    Чистка

--- fyne.io.go
+++ fyne.io.go
@@ -27,38 +27,6 @@ var (
 	weatherKeys = map[string]bool{"main": false, "wind": false, "coord": false, "weather": true, "sys": false, "clouds": false}
 )
 
-
-type WeatherInfo struct {
-  List []WeatherListItem `json:list`
-}
-
-
-
-type WeatherListItem struct {
-  Dt      int           `json:dt`
-  Main    WeatherMain   `json:main`
-  Weather []WeatherType `json:weather`
-}
-
-type WeatherMain struct {
-  Temp      float32 `json:temp`
-  FeelsLike float32 `json:feels_like`
-  Humidity  int     `json:humidity`
-}
-
-type WeatherType struct {
-  Icon string `json:icon`
-}
-// "99a2c3bc9d5f4bfde5eb78c75845393d"
-func getWeatherForecast(result interface{}) error {
-	var url = fmt.Sprintf("https://api.openweathermap.org/data/2.5/forecast?q=Voronezh&cnt=4&units=metric&appid=%s", api) // инициализируйте со своим ключом
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Print(err)
-	}
-	defer response.Body.Close()
-	return json.NewDecoder(response.Body).Decode(result)
-}
 func main() {
 	// Создаем графическую часть
     a := app.New()
